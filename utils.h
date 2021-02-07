6e61e3f6822dbb838c3112a1c901cd933a5d904f
SuRu <33urus@gmail.com>
2021-02-07 18:13:11 +0200

    added splitVersion:

--- utils.h
+++ utils.h
@@ -5,29 +5,39 @@
 
 using namespace std;
 
-void error(string s) {
-    cerr << s << endl;
+void error(string msg) {
+    cerr << msg << endl;
     exit(EXIT_FAILURE);
 }
 
 vector<string> splitAlphaNum(string s) {
-    const sregex_iterator end;
+    const regex rx("(\\d+|\\D+)");
+    const sregex_iterator last;
     vector<string> tokens;
-    regex basicRegex("(\\d+|\\D+)");
 
-    for (sregex_iterator i(s.begin(), s.end(), basicRegex); i != end; ++i) {
-        cout << (*i)[0] << endl;
+    for (sregex_iterator i(s.begin(), s.end(), rx); i != last; ++i) {
         tokens.push_back((*i)[0]);
     }
     return tokens;
 }
 
-string getOrElse(vector<string> v, int i) {
-    try {
-        return v.at(i);
-    } catch (...) {
-        return nullptr;
-    }
+string *getOrElse(vector<string> v, int i) {
+    return i < v.size() ? new string(v.at(i)) : nullptr;
+}
+
+bool isStrEmpty(const string &str) {
+    return str.empty();
+}
+
+vector<string> splitVersion(string s) {
+    const regex r("\\.|\\-");
+    const sregex_token_iterator first(s.begin(), s.end(), r, -1);
+    const sregex_token_iterator last;
+
+    vector<string> tokens = {first, last};
+    auto it = remove_if(tokens.begin(), tokens.end(), &isStrEmpty);
+    tokens.erase(it, tokens.end());
+    return tokens;
 }
 
 #endif //MVN_COMPARE_CPP_UTILS_H
