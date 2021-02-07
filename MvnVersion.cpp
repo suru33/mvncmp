0e8793ebe29563f85fe011ec54c36532e39a63ad
SuRu <33urus@gmail.com>
2021-02-07 18:59:23 +0200

    Update MvnVersion.cpp

--- MvnVersion.cpp
+++ MvnVersion.cpp
@@ -1,14 +1,10 @@
-#include <iostream>
 #include "MvnVersion.h"
 
-using namespace std;
-
 MvnVersion::MvnVersion(string version) {
-    this->version = version;
-    cout << this->allQualifiers[0][0] << endl;
+    this->version = std::move(version);
 }
 
-std::ostream &operator<<(ostream &out, MvnVersion c) {
+std::ostream &operator<<(std::ostream &out, MvnVersion c) {
     out << c.version;
     return out;
 }
