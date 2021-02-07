10a26d9292783ac04379b4dc162e5ce6209d40b5
SuRu <33urus@gmail.com>
2021-02-07 18:57:56 +0200

    Updated MvnVersion

--- MvnVersion.cpp
+++ MvnVersion.cpp
@@ -1,5 +1,14 @@
-//
-// Created by SuRu on 5-2-2021.
-//
-
+#include <iostream>
 #include "MvnVersion.h"
+
+using namespace std;
+
+MvnVersion::MvnVersion(string version) {
+    this->version = version;
+    cout << this->allQualifiers[0][0] << endl;
+}
+
+std::ostream &operator<<(ostream &out, MvnVersion c) {
+    out << c.version;
+    return out;
+}
--- MvnVersion.h
+++ MvnVersion.h
@@ -1,14 +1,34 @@
-//
-// Created by SuRu on 5-2-2021.
-//
-
 #ifndef MVN_COMPARE_CPP_MVNVERSION_H
 #define MVN_COMPARE_CPP_MVNVERSION_H
 
+#include <string>
+#include <vector>
+
+using namespace std;
 
 class MvnVersion {
+    string version;
+    int majorVersion = 0;
+    int minorVersion = 0;
+    int patchVersion = 0;
+    int qualifierPrefix = 0;
+    int qualifierType = 0;
+    int qualifierSuffix = 0;
+
+    vector<vector<string>> allQualifiers{
+            {"alpha",     "a"},
+            {"beta",      "b"},
+            {"milestone", "m"},
+            {"rc",        "cr"},
+            {"snapshot"},
+            {"",          "ga", "final"},
+            {"sp"}
+    };
+
 public:
-    MvnVersion();
+    explicit MvnVersion(string version);
+
+    friend ostream &operator<<(ostream &out, MvnVersion mvnVersion);
 };
 
 
