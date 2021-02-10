4d0a8a33257bb00777b2d56bc7271d533d099f94
SuRu <33urus@gmail.com>
2021-02-10 02:27:04 +0200

    Updated error message

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        error("usage: mvncmp <v1> <v2>\n");
+        error("usage: mvncmp <v1> <v2>");
     }
     auto *v1 = new MvnVersion(std::string(argv[1]));
     auto *v2 = new MvnVersion(std::string(argv[2]));
